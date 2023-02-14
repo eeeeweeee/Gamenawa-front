2414d8b9199797053f333b9c5db5f839ca4d7eb2
Rabongbong <didgmltn7769@naver.com>
2023-02-14 23:40:12 +0900

    feat: add routes for view

---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import SearchView from '../views/SearchView.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: SearchView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
