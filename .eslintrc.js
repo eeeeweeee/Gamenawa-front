626ce691486671299577bc7ee4afc6b794fdc4f2
Rabongbong <didgmltn7769@naver.com>
2023-02-14 23:37:56 +0900

    chore: edit lint & setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['airbnb', 'plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript'],
+  extends: ['airbnb', 'airbnb-typescript', 'plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'linebreak-style': 0,
   },
-
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    project: ['tsconfig.json'],
   },
 };
